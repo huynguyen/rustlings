f948d0e20e0c860bb93cd20d15e4a5830e39c0ca
Huy Nguyen <weehuy@gmail.com>
2023-04-13 18:57:46 -0700

    complete up to strings

--- exercises/enums/enums3.rs
+++ exercises/enums/enums3.rs
@@ -2,10 +2,12 @@
 // Address all the TODOs to make the tests pass!
 // Execute `rustlings hint enums3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 enum Message {
-    // TODO: implement the message variant types based on their usage below
+ChangeColor(i32, i32, i32),
+Echo(String),
+Move(Point),
+Quit
 }
 
 struct Point {
@@ -37,6 +39,12 @@ impl State {
     }
 
     fn process(&mut self, message: Message) {
+        match message {
+            Message::ChangeColor(r, g, b) => self.color = (r.try_into().unwrap(),g.try_into().unwrap(),b.try_into().unwrap()),
+            Message::Echo(msg) => println!( "{}", msg),
+            Message::Move(coords) => self.position = coords,
+            Message::Quit => self.quit = true,
+        }
         // TODO: create a match expression to process the different message variants
         // Remember: When passing a tuple as a function argument, you'll need extra parentheses: fn function((t, u, p, l, e))
     }
--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -1,21 +1,20 @@
 // strings3.rs
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 fn trim_me(input: &str) -> String {
+    input.trim().to_string()
     // TODO: Remove whitespace from both ends of a string!
-    ???
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    format!("{} world!", input)
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    String::from(input).replace("cars", "balloons")
 }
 
 #[cfg(test)]
